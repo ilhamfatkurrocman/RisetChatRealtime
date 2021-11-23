8b7e5d725bdac8a966e351b2bbd6719ec775ce03
Ilham fatkur rocman <ideveloper210698@gmail.com>
2021-11-23 21:33:01 +0700

    otp

--- app/src/main/java/dev/riset/chatrealtime/VerificationActivity.kt
+++ app/src/main/java/dev/riset/chatrealtime/VerificationActivity.kt
@@ -1,11 +1,27 @@
 package dev.riset.chatrealtime
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import dev.riset.chatrealtime.databinding.ActivityVerificationBinding
 
 class VerificationActivity : AppCompatActivity() {
+
+    var binding : ActivityVerificationBinding? = null
+    var auth : FirebaseAuth? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_verification)
+        binding = ActivityVerificationBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+
+        auth = FirebaseAuth.getInstance()
+        if (auth!!.currentUser != null) {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
